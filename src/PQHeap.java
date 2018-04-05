d9188d46b2c17d8d10845f2b9611e4b5a85a6eb7
Daniel <dajoh16@student.sdu.dk>
2018-04-05 14:51:24 +0200

    Genaflevering Fixes

--- src/PQHeap.java
+++ src/PQHeap.java
@@ -6,86 +6,103 @@ import java.util.Arrays;
  */
 public class PQHeap implements PQ {
 
-        private Element[] elements;
-        private int maxElements;
-        private int heapSize;
+    private Element[] elements;
+    private int heapSize;
 
-        public PQHeap(int maxElements){
-            elements = new Element[maxElements];
-            this.maxElements = maxElements;
-            heapSize = -1;
+    public PQHeap(int maxElements) {
+        elements = new Element[maxElements];
+        heapSize = -1;
 
-        }
+    }
 
     /**
-     *Extracts the root element which is the min element
+     * Extracts the root element which is the min element
+     *
      * @return
      */
     @Override
-        public Element extractMin() {
-            Element min = elements[0];
-            elements[0] = elements[heapSize];
-            heapSize--;
-            minHeapify(0);
-            return min;
-        }
+    public Element extractMin() {
+        Element min = elements[0];
+        elements[0] = elements[heapSize];
+        heapSize--;
+        minHeapify(0);
+        return min;
+    }
 
     /**
      * Inserts an element into the priority queue heap
+     *
      * @param element
      */
     @Override
-        public void insert(Element element) {
-            maxElements++;
-            heapSize++;
-            int i = heapSize;
-            elements = Arrays.copyOf(elements,maxElements);
+    public void insert(Element element) {
+        heapSize++;
+        int i = heapSize;
 
-            elements[i] = element;
-            if(elements[parent(i)] != null && elements[i] != null) {
-                while (i > 0 && elements[parent(i)].key > elements[i].key) {
-                    Element temp = elements[parent(i)];
-                    elements[parent(i)] = elements[i];
-                    elements[i] = temp;
-                    i = parent(i);
-                }
+        elements[i] = element;
+        if (elements[parent(i)] != null && elements[i] != null) {
+            while (i > 0 && elements[parent(i)].key > elements[i].key) {
+                Element temp = elements[parent(i)];
+                elements[parent(i)] = elements[i];
+                elements[i] = temp;
+                i = parent(i);
             }
         }
+    }
 
     /**
      * Rebuilds the heap based on a min heap structure. The call is recursive.
+     *
      * @param i
      */
-        private void minHeapify(int i){
-            int left = left(i);
-            int right = right(i);
-            int smallest;
-            if(left <= heapSize && elements[left].key < elements[i].key){
-                smallest = left;
-            } else {
-                smallest = i;
-            }
-            if(right <= heapSize && elements[right].key < elements[smallest].key){
-                smallest = right;
-            }
-            if(smallest != i){
-                Element temp = elements[smallest];
-                elements[smallest] = elements[i];
-                elements[i] = temp;
-                minHeapify(smallest);
-            }
+    private void minHeapify(int i) {
+        int left = left(i);
+        int right = right(i);
+        int smallest;
+        if (left <= heapSize && elements[left].key < elements[i].key) {
+            smallest = left;
+        } else {
+            smallest = i;
         }
-
-        private int left(int i){
-            return 2*i;
+        if (right <= heapSize && elements[right].key < elements[smallest].key) {
+            smallest = right;
         }
-
-        private int right(int i){
-            return 2*i+1;
-        }
-        private int parent(int i){
-            return (i/2);
+        if (smallest != i) {
+            Element temp = elements[smallest];
+            elements[smallest] = elements[i];
+            elements[i] = temp;
+            minHeapify(smallest);
         }
     }
+
+    /**
+     * returns the left node index
+     * @param i
+     * @return
+     */
+    private int left(int i) {
+        return 2 * i + 1;
+    }
+
+    /**
+     * Returns the right node index
+     * @param i
+     * @return
+     */
+    private int right(int i) {
+        return 2 * i + 2;
+    }
+
+    /**
+     *
+     * Returns -1 if at root.
+     *
+     * @param i
+     * @return
+     */
+    private int parent(int i) {
+        return (int) (Math.ceil(i / 2) - 1);
+    }
+}
 
 
